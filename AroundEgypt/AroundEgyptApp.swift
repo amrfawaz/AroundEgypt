35c2628685c7e9dc19c212931811af2420367796
Amr Fawaz <amr.abdelhakim@sigma-emea.com>
2025-03-18 11:22:00 +0200

    Initial Commit

---
+++ AroundEgypt/AroundEgyptApp.swift
@@ -0,0 +1,17 @@
+//
+//  AroundEgyptApp.swift
+//  AroundEgypt
+//
+//  Created by Amr Abd-Elhakim on 18/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct AroundEgyptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AroundEgypt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AroundEgypt
+//
+//  Created by Amr Abd-Elhakim on 18/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
